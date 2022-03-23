6106d2300edf7b80bcc4278c7944419ecc10e1e8
Manue Ortiz Hernández <manuoh09@gmail.com>
2022-03-23 01:34:23 -0600

    App name modified

--- backend/reservame_app/apps.py
+++ backend/reservame_app/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class ReservameConfig(AppConfig):
+class ReservameAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'reservame'
+    name = 'reservame_app'
