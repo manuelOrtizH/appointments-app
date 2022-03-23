29b64d5ecb80d9ba137bd797b428974ecd2c2703
Manue Ortiz Hernández <manuoh09@gmail.com>
2022-03-23 01:49:05 -0600

    Revert "App name modified"
    This reverts commit 6106d2300edf7b80bcc4278c7944419ecc10e1e8.

--- backend/reservame/apps.py
+++ backend/reservame/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class ReservameAppConfig(AppConfig):
+class ReservameConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'reservame_app'
+    name = 'reservame'
