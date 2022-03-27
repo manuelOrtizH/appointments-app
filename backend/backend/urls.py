fbb6ef948f9b5f49177bd31c097fcc9401842d2f
Manue Ortiz Hernández <manuoh09@gmail.com>
2022-03-26 20:30:56 -0600

    Serializers and routers implemented

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -14,8 +14,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from reservame import views
+
+router = routers.DefaultRouter()
+router.register(r'appointments', views.AppointmentView, 'appointment')
+router.register(r'pymes', views.PymeView, 'pyme')
+router.register(r'proffessionists', views.ProfessionistView, 'professionist')
+router.register(r'users', views.UserView, 'user')
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
 ]
--- backend/reservame/views.py
+++ backend/reservame/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
-
+from rest_framework import viewsets
+from reservame.serializers import AppointmentSerializer, PymeSerializer, ProfessionistSerializer, UserSerializer 
+from reservame.models import Appointment, Pyme, Professionist, User
 # Create your views here.
+class AppointmentView(viewsets.ModelViewSet):
+    serializer_class = AppointmentSerializer
+    queryset = Appointment.objects.all()
+
+class PymeView(viewsets.ModelViewSet):
+    serializer_class = PymeSerializer
+    queryset = Pyme.objects.all()
+
+class ProfessionistView(viewsets.ModelViewSet):
+    serializer_class = ProfessionistSerializer
+    queryset = Professionist.objects.all()
+
+class UserView(viewsets.ModelViewSet):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
