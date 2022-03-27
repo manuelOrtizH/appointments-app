ba7dddd59cc2c8b3b996f3287b53f7bf8d3feaff
Manue Ortiz Hernández <manuoh09@gmail.com>
2022-03-26 18:45:39 -0600

    Models base

--- backend/reservame/models.py
+++ backend/reservame/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
 
 # Create your models here.
+class Professionist(models.Model):
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    email = models.EmailField()
+
+    def __str__(self):
+        return f'{first_name} {last_name}'
+class Appointment(models.Model):
+    #Should be a Foreign Key, One to Many. One user could have multiple appointments
+    date = models.DateTimeField(auto_now=True) 
+    reason = models.CharField(max_length=255)
+class User(models.Model):
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    email = models.EmailField()
+    phone_number = PhoneNumber.from_string(phone_number = raw_phone, region='MX').as_e164
+    appointment = models.ForeignKey(Appointment, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{first_name} {last_name}'
+
+class Pyme(models.Model):
+    name = models.CharField(max_length=50)
+    address = models.CharField(max_length=255)
+    phone_number = PhoneNumber.from_string(phone_number = raw_phone, region='MX').as_e164
+    employees = models.IntegerField()
+    business_line = models.CharField(max_length=50)
+    professionist = models.ForeignKey(Professionist, on_delete=models.CASCADE)
+    appointment = models.ForeignKey(Appointment, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{name}'
+
+
+
+
