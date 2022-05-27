77d1c9402a203e4d7e1f561fee0ca50c110c462e
VicAnto99 <A01339529@itesm.mx>
2022-05-26 22:59:49 -0500

    Image Storage Complete
    Co-Authored-By: Raúl González Cardona <44035290+Raul5412@users.noreply.github.com>

--- frontend/src/components/profile/EditProfile.js
+++ frontend/src/components/profile/EditProfile.js
@@ -7,36 +7,66 @@ import { getUserClient } from '../.././actions/api';
 import './styles/Profile.css';
 import { ToastContainer, toast } from 'react-toastify';
 import { handleUser } from '../.././actions/api';
+import { initializeApp } from 'firebase/app';
+import { getFirestore, collection, getDocs } from 'firebase/firestore';
+import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
+import Avatar from '@mui/material/Avatar';
 
 const EditProfile = () => {
+
     const { id } = useParams();
     let navigate = useNavigate();
     const profilePictureRef = React.createRef();
     
     const [user, setUser] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
-    
+
     useEffect(async () => {
         setIsLoading(true);
         await getUserClient(id,setUser);
+        
         setIsLoading(false);
     }, []);
 
-    const profile = user;
-    console.log(profile)
-    
-    const [formData, setFormData] = useState({
-        name: profile.name,
-        last_name: profile.last_name,
-        email: profile.email,
-        phone_number: profile.phone_number,
-    });
+    const profile = user ? user :[];
+    const [image, setImage] = useState(profile ? profile:'')
+    const [formData, setFormData ] = useState(profile ?{
+        name: '',
+        last_name: '',
+        email: '',
+        phone_number: '',
+        profile_image:''
+    } : {})
 
-    const { name, last_name, email, phone_number } = formData;
+    const { name, last_name, email, phone_number, profile_image} = formData;
+
+    const storage = getStorage();
+    const storageRef = ref(storage, profile.id);
+
+    const onImageChange = e => {
+        //Lo que ahorita de diga poner
+        let img = e.target.files[0];
+        setImage(URL.createObjectURL(img));
+        console.log('form', formData);
+        uploadBytes(storageRef, img).then((snapshot) => {
+            console.log('Uploaded the image!');
+        });
+
+        console.log('formdata', formData);
+
+        setTimeout(() => {
+            getDownloadURL(storageRef).then(url => {
+                console.log('Download URL', url);
+                setFormData({...formData, profile_image: url });
+            }).catch((error) => {
+                console.log('error: ', error);
+            });
+          }, 500);
+    };
 
     const onChange = e => {
         setFormData({ ...formData, [e.target.name]: e.target.value });
-        console.log(formData);
+        console.log('form', formData);
     };
 
     const phoneValidation = (phone) => {
@@ -54,6 +84,8 @@ const EditProfile = () => {
             profile.name = name;
             profile.last_name = last_name;
             profile.phone_number = phone_number;
+            profile.profile_image = profile_image;
+
             await handleUser(profile, toast);
             //signup(name, last_name, email, phone_number, password, re_password, is_admin === 'true', toast, setAccountCreated);
             await navigate("/profile", { replace: true });
@@ -76,7 +108,6 @@ const EditProfile = () => {
                 theme='colored'
             />
                     <div className='card-header mb-4'>
-
                     <h1 className='card-title  text-center'>Editar mi perfil</h1>
                     </div>
                     <div className='card-body'>
@@ -84,6 +115,17 @@ const EditProfile = () => {
                             {!isLoading && user && 
                             <div>
                             <form onSubmit={e => onSubmit(e)}>
+                                <div className='form-group text-center'>
+                                    <div className='row text-center'>
+                                        <div className='col text-center'>
+                                            <Avatar alt="Remy Sharp" src={profile_image} style={{marginLeft: '50%', alignSelf: 'center'}} sx={{ width: 90, height: 90 }}/>
+                                        </div>
+                                        <div className='col text-center'>
+                                        <label className='btn btn-success btn-lg mr-5' htmlFor="img_id" style={{marginLeft: '10%', marginTop: '5%', padding:"1px 10px" }}>Seleccionar imagen</label>
+                                            <input id='img_id' style={{marginLeft: '20vh', visibility:"hidden"}} type='file' name='profile_image' onChange={e=>onImageChange(e)} ></input>
+                                        </div>
+                                    </div>
+                                </div>
                                 <div className='form-group'>
                                     <span className="card-text text-white">
                                         <FaUserAlt style={{color: 'white', marginRight: '5px'}}/>
@@ -129,7 +171,6 @@ const EditProfile = () => {
                                         required
                                     />
                                 </div> */}
-                                
                                 <div className='form-group'>
                                     <span className="card-text text-white">
                                         <FaPhoneAlt style={{color: 'white', marginRight: '5px'}}/>
@@ -144,7 +185,6 @@ const EditProfile = () => {
                                         required
                                     />
                                 </div>
-
                                 <hr></hr>
                                 <div className='row'>
                                     <div className='col text-center mt-2 mb-2'>
@@ -162,19 +202,13 @@ const EditProfile = () => {
                                         </Link>
                                     </div>
                                 </div>
-
-                            </form>
-                            
-                                         
+                            </form>         
                         </div>
                         }
+                        <script src="EditProfile.js"></script>
                         {isLoading && <Loading/>}
                     </div>
-                    
-                   
                 </div>
-
-                
             </article>
         </div>
     );
