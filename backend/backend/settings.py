81529efc5ba7ba9c97e3703f56192eb04c0f2f47
Manue Ortiz Hernández <manuoh09@gmail.com>
2022-05-29 03:00:20 -0500

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -126,14 +126,14 @@ DATABASES = {
 
 
 
-#monthypython15@gmail.com
-#akdicgsseabfrwke
+#reservameapp0@gmail.com
+#tvxqwioxbfxbgann
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'monthypython15@gmail.com'
-EMAIL_HOST_PASSWORD = 'akdicgsseabfrwke'
+EMAIL_HOST_USER = 'reservameapp0@gmail.com'
+EMAIL_HOST_PASSWORD = 'tvxqwioxbfxbgann'
 EMAIL_USE_TLS = True
 
 
